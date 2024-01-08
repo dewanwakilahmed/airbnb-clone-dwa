5c54ff9913574eb2c73ad212ba04eae59a9ca20d
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-08 12:20:29 +0600

    Added registerModal.onOpen function to MenuItem

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,7 +7,11 @@ import { MenuItem } from '@/app/components/navbar/MenuItem';
 
 import { useCallback, useState } from 'react';
 
+import { useRegisterModal } from '@/app/hooks/useRegisterModal';
+
 export const UserMenu = () => {
+  const registerModal = useRegisterModal();
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
@@ -39,7 +43,7 @@ export const UserMenu = () => {
           <div className='flex flex-col cursor-pointer'>
             <>
               <MenuItem onClick={() => {}} label='Login' />
-              <MenuItem onClick={() => {}} label='Sign up' />
+              <MenuItem onClick={registerModal.onOpen} label='Sign up' />
             </>
           </div>
         </div>
