1a12f0ce849c204e257a991821ea8f51c9536a55
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-09 19:09:09 +0600

    Hydrated Avatar component with src prop

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -43,7 +43,7 @@ export const UserMenu: FC<UserMenuProps> = ({ currentUser }) => {
         >
           <AiOutlineMenu />
           <div className='hidden md:block'>
-            <Avatar />
+            <Avatar src={currentUser?.image} />
           </div>
         </div>
       </div>
