db407b3f734698b503d3757c834f23d0f027f272
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-09 19:07:39 +0600

    Added GitHub authentication to RegisterModal  Component

--- app/components/modal/RegisterModal.tsx
+++ app/components/modal/RegisterModal.tsx
@@ -1,12 +1,13 @@
 'use client';
 
+import { useState } from 'react';
+import { signIn } from 'next-auth/react';
+
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
 import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
-
-import { useCallback, useState } from 'react';
 
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
@@ -88,7 +89,7 @@ export const RegisterModal = () => {
         outline
         label='Continue with GitHub'
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn('github')}
       />
       <div className='text-neutral-500 text-center mt-4 font-light'>
         <div className='justify-center flex flex-row items-center gap-2'>
