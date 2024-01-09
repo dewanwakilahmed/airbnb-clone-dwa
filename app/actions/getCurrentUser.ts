1d120500efced90be9b60a57c3a97271492a5ed1
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-09 16:44:28 +0600

    Created getCurrentUser Server Action

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,38 @@
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import prisma from '@/app/libs/prismadb';
+
+export const getSession = async () => {
+  return await getServerSession(authOptions);
+};
+
+const getCurrentUser = async () => {
+  try {
+    const session = await getSession();
+
+    if (!session?.user?.email) {
+      return null;
+    }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string,
+      },
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return {
+      ...currentUser,
+      createdAt: currentUser.createdAt.toISOString(),
+      updatedAt: currentUser.updatedAt.toISOString(),
+      emailVerified: currentUser.emailVerified?.toISOString() || null,
+    };
+  } catch (error: any) {
+    return null;
+  }
+};
+
+export default getCurrentUser;
