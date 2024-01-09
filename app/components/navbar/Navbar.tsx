0a7e87bf11bd43c59b65cc0a5c42592057f9be10
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-09 16:46:14 +0600

    Hydrated UserMenu Component with currentUser

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,11 +1,18 @@
 'use client';
 
+import { FC } from 'react';
+import { User } from '@prisma/client';
+
 import { Container } from '@/app/components/Container';
 import { Logo } from '@/app/components/navbar/Logo';
 import { Search } from '@/app/components/navbar/Search';
 import { UserMenu } from '@/app/components/navbar/UserMenu';
 
-export const Navbar = () => {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+export const Navbar: FC<NavbarProps> = ({ currentUser }) => {
   return (
     <div className='fixed w-full bg-white z-10 shadow-sm'>
       <div className='py-4 border-b-[1px]'>
@@ -13,7 +20,7 @@ export const Navbar = () => {
           <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
             <Logo />
             <Search />
-            <UserMenu />
+            <UserMenu currentUser={currentUser} />
           </div>
         </Container>
       </div>
