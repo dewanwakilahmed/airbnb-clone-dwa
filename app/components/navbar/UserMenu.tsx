06566e441cee6def0d1da26beb0bbaee15f8818e
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-01-10 19:03:46 +0600

    Created onRent function

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -11,6 +11,7 @@ import { MenuItem } from '@/app/components/navbar/MenuItem';
 
 import { useRegisterModal } from '@/app/hooks/useRegisterModal';
 import { useLoginModal } from '@/app/hooks/useLoginModal';
+import { useRentModal } from '@/app/hooks/useRentModal';
 
 import { signOut } from 'next-auth/react';
 
@@ -21,6 +22,7 @@ interface UserMenuProps {
 export const UserMenu: FC<UserMenuProps> = ({ currentUser }) => {
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
+  const rentModal = useRentModal();
 
   const [isOpen, setIsOpen] = useState(false);
 
@@ -28,12 +30,18 @@ export const UserMenu: FC<UserMenuProps> = ({ currentUser }) => {
     setIsOpen((value) => !value);
   }, []);
 
+  const onRent = useCallback(() => {
+    if (!currentUser) return loginModal.onOpen();
+
+    rentModal.onOpen();
+  }, [currentUser, loginModal, rentModal]);
+
   return (
     <div className='relative'>
       <div className='flex flex-row items-center gap-3'>
         <div
           className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'
-          onClick={() => {}}
+          onClick={onRent}
         >
           Airbnb your home
         </div>
